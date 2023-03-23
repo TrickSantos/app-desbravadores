be83ad9d9d1ac200c8349388bdb2a9b8f3d788a3
Patrick Santos <patrick.tafa@gmail.com>
2023-03-23 17:22:41 -0400

    :tada: Commit inicial

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <div className="w-full h-screen">
+      <div className="w-full xl:h-2/6 md:h-1/5">
+        <img className="w-full h-full object-cover" src="https://picsum.photos/2000/1000" alt="" />
+      </div>
+      <div className="w-full h-full p-4">
+        <div className="w-full flex flex-col space-y-2">Desbravadores</div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
